ec8dac68285b35b621d5d1964403b1b7f8edfd55
jaimefo1 <jaimefo1@gmail.com>
2020-04-02 20:08:31 +0200

    ver 0.1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel jaime
+                    Laravel ctiro
                 </div>
 
                 <div class="links">
