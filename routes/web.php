6723da3f0b5f87a325631100bf23234f2e88ce4d
jaimefo1 <jaimefo1@gmail.com>
2020-04-10 12:25:48 +0200

    nada

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,52 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+#region pruebas 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('welcome', function(){
-    return "hoal mundo";
+
+Route::view('/', 'welcome');
+
+
+Route::get('welcome', function () {
+    return "hola mundo";
 });
+
+Route::get('bye', function () {
+    return 'hasta luego lucas';
+});
+
+Route::match(['get', 'post'], 'numeroAlAzar', function () {
+    return rand(1, 10);
+});
+
+
+//parametros opcionales y con patrones
+Route::pattern('usuario', '\d+');
+
+Route::get('usuario/{usuario?}', function ($usuario = null) {
+    if ($usuario === null) {
+        return "sin usuario";
+    };
+    return "eres el usuario $usuario";
+});
+
+//una ruta para convertir de hexadecimal a decimal
+
+//el patron de hexadecimal 
+Route::pattern('valorHex', '[0-9A-Fa-f]+');
+Route::get('conversor/hexadecimal/{valorHex}', function ($valorHex) {
+    return hexdec($valorHex);
+});
+
+Route::get(
+    'conversor/hexadecimal/{valorHex}',
+    ['as' => 'convertir', function ($valorHex) {
+        return hexdec($valorHex);
+    }]
+);
+*/
+#endregion
